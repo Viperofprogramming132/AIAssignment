bd00ff223775413a6d1a28d589c8f773f5f8b36b
Aidan Wiseman <365115@edu.cwa.ac.uk>
2018-10-10 16:58:48 +0100

    Reads in the files

---
+++ AIAssignment/BayesainNetwork.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIAssignment
+{
+    using System.IO;
+
+    public class BayesainNetwork
+    {
+        private List<FileInfo> m_TrainingDataFiles;
+
+        /// <summary>
+        /// Starts the training process
+        /// </summary>
+        public void StartTraining()
+        {
+            this.m_TrainingDataFiles = new List<FileInfo>();
+            this.GetTrainingData();
+        }
+
+        private void GetTrainingData()
+        {
+            bool badInput = false;
+            bool retry = false;
+            Console.Clear();
+
+            //Get the directory and all the files inside of it
+            DirectoryInfo trainingDirectory = new DirectoryInfo(Directory.GetCurrentDirectory() + "\\training");
+            FileInfo[] possibleTrainingFiles = trainingDirectory.GetFiles("*.txt");
+
+            //Write all the files inside of the directory
+            Console.WriteLine("Possible files found: " + possibleTrainingFiles.Length + ".\n");
+            for (int i = 0;i < possibleTrainingFiles.Length; ++i)
+            {
+                Console.WriteLine(i + 1 + ") " + possibleTrainingFiles[i]);
+            }
+
+            do
+            {
+                retry = false;
+                badInput = false;
+                Console.WriteLine("Please select which you wish to use (Separate selection with a comma ',').");
+
+                string input = Console.ReadLine();
+
+                string[] selections = input.Split(',');
+
+                foreach (string selection in selections)
+                {
+                    //Make sure they only input numbers between the commas
+                    try
+                    {
+                        this.m_TrainingDataFiles.Add(possibleTrainingFiles[int.Parse(selection) - 1]);
+                    }
+                    catch
+                    {
+                        badInput = true;
+                    }
+                }
+
+                //Write out selected files
+                Console.WriteLine("Selected files are:");
+                foreach (FileInfo file in this.m_TrainingDataFiles)
+                {
+                    Console.WriteLine(file.Name);
+                }
+
+                //Ensure there is no bad inputs
+                if (badInput)
+                {
+                    Console.WriteLine("One or more of the inputs were not added do you wish to try again (y/n)");
+                    char cont = char.Parse(Console.ReadLine());
+
+                    cont = Char.ToLower(cont);
+
+                    //Check if they wish to retry otherwise continue
+                    switch (cont)
+                    {
+                        case 'y':
+                            retry = true;
+                            break;
+                    }
+                }
+            }
+            while (retry);
+        }
+    }
+}
--- AIAssignment/Program.cs
+++ AIAssignment/Program.cs
@@ -10,6 +10,43 @@ namespace AIAssignment
     {
         static void Main(string[] args)
         {
+            char input;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Please Select an Option:\n");
+                Console.WriteLine("A) Undertake training");
+                Console.WriteLine("B) Undertake Classification");
+                Console.WriteLine("Q) Exit");
+
+                char.TryParse(Console.ReadLine(), out input);
+
+
+                if (input != null)
+                {
+                    input = Char.ToLower(input);
+                    switch (input)
+                    {
+                        case 'a':
+                            //TODO: Training
+                            BayesainNetwork b = new BayesainNetwork();
+                            b.StartTraining();
+                            break;
+                        case 'b':
+                            //TODO: Classification
+                            break;
+                        case 'q':
+                            //TODO: Exit
+                            break;
+                        default:
+                            Console.WriteLine("Please only input A, B or Q to exit (not case sensitive).");
+                            break;
+                    }
+                }
+
+                Console.ReadKey();
+            }
+            while (input != 'q');
         }
     }
 }
